11cc85d958c05e90b2dbf1810413b1b8a8504017
jhmin3101 <jhmin3101@tmon.co.kr>
2021-05-28 14:15:08 +0900

    commit

--- js/jeonghoo/5.js
+++ js/jeonghoo/5.js
@@ -59,18 +59,18 @@ console.log(obj); //{x:1,y:2}
 // child.sayHi();
 
 
-// const parent = {
-//   name: 'parent',
-//   sayHi() {
-//     console.log('Hi! ' + this.name);
-//   }
-// };
+const parent = {
+  name: 'parent',
+  sayHi() {
+    console.log('Hi! ' + this.name);
+  }
+};
 
-// const child = {
-//   // child 객체의 프로토타입 객체에 parent 객체를 바인딩하여 상속을 구현한다.
-//   __proto__: parent,
-//   name: 'child'
-// };
+const child = {
+  // child 객체의 프로토타입 객체에 parent 객체를 바인딩하여 상속을 구현한다.
+  __proto__: parent,
+  name: 'child'
+};
 
-// parent.sayHi(); // Hi! parent
-// child.sayHi();  // Hi! child
+parent.sayHi(); // Hi! parent
+child.sayHi();  // Hi! child
