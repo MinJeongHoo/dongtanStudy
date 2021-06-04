c6a5113cc7a838af454177462398dbde9aa48594
jhmin3101 <jhmin3101@tmon.co.kr>
2021-06-04 10:48:12 +0900

    수정

--- js/jeonghoo/7.js
+++ js/jeonghoo/7.js
@@ -78,3 +78,38 @@ class Person {
 // 인스턴스 생성
 const me = new Person('Lee');
 console.log(me); // Person {_name: "Lee"}
+
+//클래스 몸체(class body)에는 메소드만 선언할 수 있다. 클래스 바디에 클래스 필드(멤버 변수)를 선언하면 문법 에러(SyntaxError)가 발생한다.
+class Foo {
+  name = ''; // SyntaxError
+
+  constructor() {}
+}
+
+
+
+class Foo {
+  x = 1;            // Field declaration
+  #p = 0;           // Private field
+  static y = 20;    // Static public field
+  static #sp = 30;  // Static private field
+  // 2019/5 : Chrome 미구현
+  // static #sm() {    // Static private method
+  //   console.log('static private method');
+  // }
+
+  bar() {
+    this.#p = 10; // private 필드 참조
+    // this.p = 10; // 새로운 public p 필드를 동적 추가한다.
+    return this.#p;
+  }
+}
+
+const foo = new Foo();
+console.log(foo); // Foo&nbsp;{#p: 10, x: 1}
+
+console.log(foo.x); // 1
+// console.log(foo.#p); // SyntaxError: Undefined private field #p: must be declared in an enclosing class
+console.log(Foo.y); // 20
+// console.log(Foo.#sp); // SyntaxError: Undefined private field #sp: must be declared in an enclosing class
+console.log(foo.bar()); // 10
